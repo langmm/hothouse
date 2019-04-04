87ff148a2865032872f7c469ea358d9c7712dc8c
Matthew Turk <matthewturk@gmail.com>
2019-04-04 11:16:26 -0500

    Adding versioneer

--- hothouse/__init__.py
+++ hothouse/__init__.py
@@ -5,3 +5,7 @@
 __author__ = """Matthew Turk"""
 __email__ = 'matthewturk@gmail.com'
 __version__ = '0.1.0'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions

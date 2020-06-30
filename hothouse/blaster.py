033864ee9f078ed5f1e0cca9d0bb2942b771d375
Meagan Lang <langmm.astro@gmail.com>
2020-06-30 16:30:51 -0700

    Change ray to pass-by-ref in callback and add class for multi-bounce collision that gathers information for each bounce.

--- hothouse/blaster.py
+++ hothouse/blaster.py
@@ -10,7 +10,7 @@ import pvlib
 from .traits_support import check_dtype, check_shape
 
 from hothouse import sun_calc
-from .ray_callbacks import RayCollisionPrinter
+from .ray_callbacks import RayCollisionPrinter, RayCollisionMultiBounce
 
 # pyembree receives origins and directions.
 
@@ -26,6 +26,7 @@ class RayBlaster(traitlets.HasTraits):
     directions = traittypes.Array().valid(check_shape(None, 3), check_dtype("f4"))
     intensity = traitlets.CFloat(1.0)
     diffuse_intensity = traitlets.CFloat(0.0)
+    multibounce = traitlets.CBool(False)
 
     @property
     def ray_intensity(self):
@@ -33,12 +34,19 @@ class RayBlaster(traitlets.HasTraits):
         return self.intensity / self.origins.shape[0]
 
     def cast_once(self, scene, verbose_output=False, query_type=QueryType.DISTANCE):
+        if self.multibounce:
+            callback_handler = RayCollisionMultiBounce(self.origins.shape[0], 10)
+        else:
+            callback_handler = None
         output = scene.embree_scene.run(
             self.origins,
             self.directions,
             query=query_type._value_,
             output=verbose_output,
+            callback_handler=callback_handler
         )
+        if self.multibounce:
+            output['bounces'] = callback_handler.bounces
         return output
 
     def compute_distance(self, scene):
@@ -137,6 +145,7 @@ class SunRayBlaster(OrthographicRayBlaster):
     solar_azimuth = traitlets.CFloat()
     solar_distance = traitlets.CFloat()
     _solpos_info = traittypes.DataFrame()
+    multibounce = traitlets.CBool(True)
 
     @traitlets.default("_solpos_info")
     def _solpos_info_default(self):
--- hothouse/ray_callbacks.pyx
+++ hothouse/ray_callbacks.pyx
@@ -1,10 +1,113 @@
 # cython: language=c++
 
+cimport numpy as np
+import numpy as np
 from pyembree.callback_handler cimport \
     RayCollisionCallback, CALLBACK_TERMINATE, CALLBACK_CONTINUE
+from pyembree.rtcore_geometry cimport RTC_INVALID_GEOMETRY_ID
 from pyembree.rtcore_ray cimport RTCRay
 
 cdef class RayCollisionPrinter(RayCollisionCallback):
-    cdef int callback(self, RTCRay ray):
+    cdef int callback(self, RTCRay &ray):
         print("Hi!", ray.geomID)
         return CALLBACK_TERMINATE
+
+
+cdef class RayCollisionMultiBounce(RayCollisionCallback):
+
+    cdef int iray
+    cdef int nray
+    cdef int maxbounce
+    cdef public np.int32_t[:] nbounce
+    cdef public np.int32_t[:,:] primID
+    cdef public np.int32_t[:,:] geomID
+    cdef public np.int32_t[:,:] instID
+    cdef public np.float32_t[:,:] tfars
+    cdef public np.float32_t[:,:,:] Ng
+    cdef public np.float32_t[:,:,:] ray_dir
+    cdef public np.float32_t[:,:] power
+
+    def __cinit__(self, int nray, int maxbounce):
+        self.iray = 0
+        self.nray = nray
+        self.maxbounce = maxbounce
+        self.nbounce = np.zeros(nray, dtype="int32")
+        self.primID = -1 * np.ones((nray, maxbounce), dtype="int32")
+        self.geomID = -1 * np.ones((nray, maxbounce), dtype="int32")
+        self.instID = -1 * np.ones((nray, maxbounce), dtype="int32")
+        self.tfars = np.empty((nray, maxbounce), dtype="float32")
+        self.Ng = np.empty((nray, maxbounce, 3), dtype="float32")
+        self.ray_dir = np.empty((nray, maxbounce, 3), dtype="float32")
+        self.power = np.ones((nray, maxbounce), dtype="float32")
+
+    cdef void redirect_ray(self, RTCRay &ray):
+        cdef int i
+        cdef float nu, ux, uy, uz
+        cdef float new_dir[3]
+	# Move origin to point of intersection
+        for i in range(3):
+            ray.org[i] = ray.org[i] + ray.dir[i] * ray.tfar
+        nu = np.sqrt(ray.Ng[0] * ray.Ng[0]
+                     + ray.Ng[1] * ray.Ng[1]
+                     + ray.Ng[2] * ray.Ng[2])
+        nx = ray.Ng[0] / nu
+        ny = ray.Ng[1] / nu
+        nz = ray.Ng[2] / nu
+	# Rotate inverse of original direction 180 deg around surface
+	# normal to get new direction
+        new_dir[0] = -(ray.dir[0] * (-1.0 - (2.0 * nx * nx))
+                       + ray.dir[1] * (-2.0 * nx * ny)
+                       + ray.dir[2] * (-2.0 * nx * nz))
+        new_dir[1] = -(ray.dir[0] * (-2.0 * ny * nx)
+                       + ray.dir[1] * (-1.0 - (2.0 * ny * ny))
+                       + ray.dir[2] * (-2.0 * ny * nz))
+        new_dir[2] = -(ray.dir[0] * (-2.0 * nz * nx)
+                       + ray.dir[1] * (-2.0 * nz * ny)
+                       + ray.dir[2] * (-1.0 - (2.0 * nz * nz)))
+        for i in range(3):
+            ray.dir[i] = new_dir[i]
+            # Offset origin slightly along new path to prevent
+            # intersecting at new origin
+            ray.org[i] = ray.org[i] + 2.0 * ray.dir[i] * np.finfo(np.float32).eps
+
+    @property
+    def bounces(self):
+        out = {'nbounce': np.asarray(self.nbounce),
+               'primID': np.asarray(self.primID),
+               'geomID': np.asarray(self.geomID),
+               'instID': np.asarray(self.instID),
+               'tfars': np.asarray(self.tfars),
+               'Ng': np.asarray(self.Ng),
+               'ray_dir': np.asarray(self.ray_dir),
+               'power': np.asarray(self.power)}
+        return out
+
+    cdef int callback(self, RTCRay &ray):
+        cdef int idx
+        if ray.geomID == RTC_INVALID_GEOMETRY_ID:
+            self.iray += 1
+            return CALLBACK_TERMINATE
+        if self.nbounce[self.iray] >= self.maxbounce:
+            self.iray += 1
+            return CALLBACK_TERMINATE
+        # print("Hi!", self.iray, ray.geomID, ray.tnear, ray.tfar, ray.primID, ray.org, ray.dir)
+        # Record ray parameters for bounce
+        idx = self.nbounce[self.iray]
+        self.nbounce[self.iray] += 1
+        self.primID[self.iray, idx] = ray.primID
+        self.geomID[self.iray, idx] = ray.geomID
+        self.instID[self.iray, idx] = ray.instID
+        self.tfars[self.iray, idx] = ray.tfar
+        for i in range(3):
+            self.Ng[self.iray, idx, i] = ray.Ng[i]
+            self.ray_dir[self.iray, idx, i] = ray.dir[i]
+        # TODO: Update power based on transmittance/reflectance
+        # Redirect ray
+        self.redirect_ray(ray)
+        # Reset ray parameters
+        ray.tnear = 0.0
+        ray.tfar = 1e37
+        ray.geomID = RTC_INVALID_GEOMETRY_ID
+        ray.primID = RTC_INVALID_GEOMETRY_ID
+        ray.instID = RTC_INVALID_GEOMETRY_ID
+        return CALLBACK_CONTINUE
